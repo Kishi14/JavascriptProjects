2263a3d0948e1df233922b049342de6217c09a83
Kishore <kishoreraghavendra14@gmail.com>
2019-12-10 11:41:21 +0530

    My Calculator App v1.0

--- myCalcApp/assets/scripts/app.js
+++ myCalcApp/assets/scripts/app.js
@@ -1,1 +1,43 @@
-alert("Hello World");
+let userInp;
+let result = 0;
+// Function to get user Input
+function readUserInput(){
+    return userInput.value;
+}
+
+outputResult(result,"");
+
+//Hander for event listeners
+function addFuncHandler(){
+    userInp = Number(readUserInput());
+    const curVal = result;
+    result += userInp;
+    outputResult(result,`${curVal} + ${userInp} = ${result}`); 
+}
+
+function subFuncHandler(){
+    userInp = Number(readUserInput());
+    const curVal = result;
+    result -= userInp;
+    outputResult(result,`${curVal} - ${userInp} = ${result}`);
+}
+
+function multiFuncHandler(){
+    userInp = Number(readUserInput());
+    const curVal = result;
+    result *= userInp;
+    outputResult(result,`${curVal} * ${userInp} = ${result}`);
+}
+
+function divFuncHandler(){
+    userInp = Number(readUserInput());
+    const curVal = result;
+    result /= userInp;
+    outputResult(result,`${curVal} / ${userInp} = ${result}`);
+}
+
+// Even Listener
+addBtn.addEventListener('click', addFuncHandler);
+subtractBtn.addEventListener('click', subFuncHandler);
+multiplyBtn.addEventListener('click', multiFuncHandler);
+divideBtn.addEventListener('click', divFuncHandler);
